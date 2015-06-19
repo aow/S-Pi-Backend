f7f2ac30dc5982477ea34f5365992b721cb55741
Jason <jrm@Dagon.local>
2015-06-19 13:06:03 -0700

    (BACK-17) Added proper routes for our data, and setup the bridged events endpoint.

--- src/main/java/edu/pdx/spi/verticles/Server.java
+++ src/main/java/edu/pdx/spi/verticles/Server.java
@@ -1,21 +1,45 @@
 package edu.pdx.spi.verticles;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.Route;
+import io.vertx.ext.web.handler.sockjs.BridgeEvent;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+
+import edu.pdx.spi.handlers.PatientsHandler;
+import edu.pdx.spi.handlers.StreamingNumericalHandler;
+import edu.pdx.spi.handlers.StreamingWaveformHandler;
 
 public class Server extends AbstractVerticle {
-  Router router = Router.router(vertx);
-
   @Override
   public void start() {
-    router.route("/").handler(routingContext -> {
-      HttpServerResponse resp = routingContext.response();
+    Router router = Router.router(vertx);
 
-      resp.putHeader("content-type", "text/plain");
-      resp.end("Hello!");
-    });
+    // Patient data routes
+    PatientsHandler ph = new PatientsHandler();
+    router.route("/patients/:id").handler(ph);
+    router.route("/patients").handler(ph);
+
+    // Numerical stream endpoints
+    StreamingNumericalHandler sn = new StreamingNumericalHandler();
+    router.route("/stream/numerical/:type/:id").handler(sn);
+
+    // Waveform stream endpoints
+    StreamingWaveformHandler sw = new StreamingWaveformHandler();
+    router.route("/stream/waveform/:type/:id").handler(sw);
+
+    // Bridged eventbus permissions
+    BridgeOptions options = new BridgeOptions();
+    options.addOutboundPermitted(new PermittedOptions().setAddressRegex(".+\\..+"));
+
+    router.route("/stream").handler(SockJSHandler.create(vertx).bridge(options, event -> {
+      if (event.type() == BridgeEvent.Type.SOCKET_CREATED) {
+        System.out.println("Socket created");
+      }
+
+      event.complete(true);
+    }));
 
     vertx.createHttpServer().requestHandler(router::accept).listen(9999);
     System.out.println("Server listening on port 9999.");
