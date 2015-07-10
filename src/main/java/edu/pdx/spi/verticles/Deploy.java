cc874f19d8ffb79f28c1409c44b82fd53d74e4b5
weisensee <lucas.weisensee@gmail.com>
2015-07-10 13:13:14 -0700

    Merged jason's backend changes with working watch version

--- src/main/java/edu/pdx/spi/verticles/Deploy.java
+++ src/main/java/edu/pdx/spi/verticles/Deploy.java
@@ -10,17 +10,13 @@ public class Deploy extends AbstractVerticle {
     DeploymentOptions workerOptions = new DeploymentOptions().setWorker(true).setConfig(this.config());
     DeploymentOptions normalOptions = new DeploymentOptions().setConfig(this.config());
 
-<<<<<<< HEAD
-    vertx.deployVerticle("edu.pdx.spi.verticles.DataSource");
-    vertx.deployVerticle("edu.pdx.spi.verticles.Server");
-    vertx.deployVerticle("edu.pdx.spi.verticles.Alerts");
-=======
+
     if (!this.config().getBoolean("debug")) {
       vertx.deployVerticle("edu.pdx.spi.verticles.SstoreConnector", workerOptions);
     }
 
     vertx.deployVerticle("edu.pdx.spi.verticles.DataSource", normalOptions);
+    vertx.deployVerticle("edu.pdx.spi.verticles.Alerts", workerOptions);
     vertx.deployVerticle("edu.pdx.spi.verticles.Server", normalOptions);
->>>>>>> 3006ebe918c2dc96aa965fb9c9a75e49204376a8
   }
 }
