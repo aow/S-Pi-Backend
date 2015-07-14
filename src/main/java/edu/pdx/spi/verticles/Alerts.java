e8b42b0cefcc01ebdf486f261014d15d3c89b6da
weisensee <lucas.weisensee@gmail.com>
2015-07-14 16:56:53 -0700

    Google Cloud Messenger base level implementation. Needs to be cleaned up, integrated with sstore alerts and made for multiple users

--- src/main/java/edu/pdx/spi/verticles/Alerts.java
+++ src/main/java/edu/pdx/spi/verticles/Alerts.java
@@ -1,6 +1,8 @@
 package edu.pdx.spi.verticles;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.pdx.spi.GcmContent;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.http.HttpMethod;
@@ -11,15 +13,14 @@ import org.apache.commons.io.IOUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
 
 
 public class Alerts extends AbstractVerticle {
   public static final String API_KEY = "AIzaSyBe-3d2hooDYXHHQt5Rb8Qo4wn0vRVRMNE";
+    String UserKey;
+  int port = 9996;
   EventBus eb;
 
   @Override
@@ -30,57 +31,114 @@ public class Alerts extends AbstractVerticle {
 
     System.out.println("running alerts simulation");
 
-    // retrieve and save registration token
-    // TODO: send registration token from mobile if we want to keep track of users
+    // get registration code from app
+    getRegistrationCode();
 
-    // send message to client at specified token
-    sendMessage( "ALERT:watch GCM testing message for S-PI", "/topics/global");
+    // Sends a new alert every 20 seconds
+    vertx.setPeriodic(20000, id -> {
+      // send message to client at specified token
+      System.out.println("Sending GCM Message");
+
+        // create message to send to watch user
+        GcmContent content = createContent();
+
+        // send message to user
+        sendMessage(content, API_KEY);
+    });
+  }
+  private void getRegistrationCode() {
+      String msg;
+      try {
+          ServerSocket client = new ServerSocket(port);
+          // TODO: implement this as a while loop or some construct to constantly listen for new users
+          if(true)   {
+              Socket connectionSocket = client.accept();
+              BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+              DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+              msg = inFromClient.readLine();
+              System.out.println("Received: " + msg);
+              String resp = msg.toUpperCase() + '\n';
+              outToClient.writeBytes("thanks for the registration token!");
+
+              // TODO: reimplement this as a mapping for multiple users
+              // save registration token to global var for future use
+              UserKey = msg;
+          }
+
+      } catch (Exception e) {
+          System.out.print("getRegistrationCode ERROR:" + e);
+          System.exit(1);
+      }
+
   }
 
-  private void sendMessage(String toSend, String destination) {
-    try {
-      // Prepare JSON containing the GCM message content. What to send and where to send.
-      JSONObject jGcmData = new JSONObject();
-      JSONObject jData = new JSONObject();
+    // creates message content to be sent to watch
+    public GcmContent createContent(){
 
-        try {
-            // message to send to watch
-            jData.put("message", toSend);
+        GcmContent c = new GcmContent();
 
-            // send GCM message to destination passed in by arg
-            jGcmData.put("to", destination);
-            //  jGcmData.put("to", "/topics/global");
+        c.addRegId(UserKey);
+        c.createData("SPI ALERT!", "***WATCH DEMO:This is the message section of the GCM message***");
 
-            // add data from message json obj to gcm data obj (correct formatting for GCM)
-            jGcmData.put("data", jData);
-        } catch (JSONException e) {
-            System.out.println("JSON exception occurred," + e);
-            System.out.println("check that JSON func use is correct");
+        return c;
+    }
+    private void sendMessage(GcmContent content, String apiKey) {
+        try{
+
+            // 1. URL
+            URL url = new URL("https://android.googleapis.com/gcm/send");
+
+            // 2. Open connection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // 3. Specify POST method
+            conn.setRequestMethod("POST");
+
+            // 4. Set the headers
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Authorization", "key="+apiKey);
+
+            conn.setDoOutput(true);
+
+            // 5. Add JSON data into POST request body
+
+            // 5.1 Use Jackson object mapper to convert Contnet object into JSON
+            ObjectMapper mapper = new ObjectMapper();
+
+            // 5.2 Get connection output stream
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+
+            // 5.3 Copy Content "JSON" into
+            mapper.writeValue(wr, content);
+
+            // 5.4 Send the request
+            wr.flush();
+
+            // 5.5 close
+            wr.close();
+
+            // 6. Get the response
+            int responseCode = conn.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // 7. Print result
+            System.out.println(response.toString());
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-      // Create connection to send GCM Message request.
-      URL url = new URL("https://android.googleapis.com/gcm/send");
-      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-      conn.setRequestProperty("Authorization", "key=" + API_KEY);
-      conn.setRequestProperty("Content-Type", "application/json");
-      conn.setRequestMethod("POST");
-      conn.setDoOutput(true);
-
-      // Send GCM message to Watch
-      OutputStream outputStream = conn.getOutputStream();
-      outputStream.write(jGcmData.toString().getBytes());
-
-      // Read GCM response.
-      InputStream inputStream = conn.getInputStream();
-      String resp = IOUtils.toString(inputStream);
-      System.out.println(resp);
-      System.out.println("message above ^^ is from Watch, if correct, GCM is working correctly");
     }
-    // catch errors in GCM process
-    catch (IOException e) {
-        System.out.println("Unable to send GCM message.");
-        System.out.println("check that the device's registration token is correct.");
-        e.printStackTrace();
-    }
-  }
 }
