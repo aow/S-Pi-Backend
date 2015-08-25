63f652364506f08979fdc7a9c1ac23b2ad218023
Jason <jmu2@pdx.edu>
2015-08-25 15:38:21 -0700

    Temp fix for invalid data

--- src/main/java/edu/pdx/spi/handlers/PatientsHandler.java
+++ src/main/java/edu/pdx/spi/handlers/PatientsHandler.java
@@ -4,14 +4,20 @@ import io.vertx.core.Handler;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.ext.web.RoutingContext;
 
+import java.util.Objects;
+
 public final class PatientsHandler implements Handler<RoutingContext> {
   public void handle(RoutingContext rc) {
     EventBus eb = rc.vertx().eventBus();
-    String id = rc.request().getParam("id");
+    String id;
     try {
-      System.out.println(Integer.valueOf(rc.request().getParam("id")));
+      id = rc.request().getParam("id");
+      if (Objects.nonNull(id)) {
+        Integer.parseInt(id);
+      }
     } catch (Exception e) {
       rc.response().end();
+      return;
     }
     if (id == null) {
       eb.send("patients", "", m -> {
